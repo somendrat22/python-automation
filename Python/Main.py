899bdf666fed9741407cd60e5666c8c7b30af59a
Somendra Tiwari <somadritiwari@Somadris-MacBook-Air.local>
2025-01-21 22:45:11 +0530

    Python automation goduck created

---
+++ Python/Main.py
@@ -0,0 +1,16 @@
+# count = 0
+# while count <= 10: # 0<=10
+#     print(count)
+#     print("Muskan")
+#     count = count + 1
+   
+#0
+#Muskan
+#1
+
+
+n = int(input("Enter value fort n: "))
+
+sum = (n*(n+1))/2
+
+print(sum)
